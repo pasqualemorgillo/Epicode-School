a2444aaf7d53e514160362ee9e88a05b3cb6abe7
pasqualemorgillo <119693866+pasqualemorgillo@users.noreply.github.com>
2023-02-06 15:54:59 +0100

    Add files via upload

---
+++ week2/day1/assets/js/naming.js
@@ -0,0 +1,11 @@
+var animaleUno = 'Giraffa';
+console.log(animaleUno);
+
+var AnimaleDue = 'Cane';
+console.log(AnimaleDue);
+
+var animale_due = 'Lupo';
+console.log(animale_due);
+
+var animalequattro = 'vitello';
+console.log(animalequattro);
---
+++ week2/day1/assets/js/script.js
@@ -0,0 +1,7 @@
+window.alert('Primo Alert');
+
+window.alert('Secondo Alert');
+
+document.getElementById('corpo').innerHTML = '<div id="container"> <h2>Iniziamo la nostra pratica</h2> <p>output scritto da Javascript </p> </div>'
+
+document.write('<div id="container"> <p>pantera scritto da Javascript </p> </div>')
